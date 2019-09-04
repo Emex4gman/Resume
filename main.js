f505fd68aaac110f2276ad193ca6e55923b95203
EMEX <emex4gman@gmail.com>
2019-09-04 22:41:02 +0300

    updated

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ var message = document.querySelector("#message");
 
 
 
-var checkEmail = () => {
-    // console.log("hello");
-    var field = email.value;
-    var a = field.toString();
-    console.log(a);
-    // const field = "emex4gman@gmail.com";
+// var checkEmail = () => {
+//     console.log("hello");
+//     var field = email.value;
+//     var a = field.toString();
+//     console.log(a);
+//     const field = "emex4gman@gmail.com";
     // const rejex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     // const isValid = rejex.test(field)
     // if (isValid === true) {
@@ -21,15 +21,32 @@ var checkEmail = () => {
     // };
 
     // return isValid;
+//}
 
+function checkForm() {
+    var fullName = document.querySelector("#fullname");
+    var message = document.querySelector("#message");
+
+    if (fullName.value.length < 4) {
+        alert('You full Name length needs to be at least 4 characters!');
+        return false;
+    } else if (message.value.length < 20) {
+        alert('Message length needs to be at least 20 characters!');
+        return false;
+    } else {
+        alert('Form submitted successfully');
+
+        return true;
+
+    }
 }
 
 
 
 
 
-submit.addEventListener("click", () => {
-    alert("form is not read");
-    window.location.assign("chukwuemeka.html");
-    window.location.reload(true);
-})
+// submit.addEventListener("click", () => {
+//     alert("form is not read");
+//     window.location.assign("chukwuemeka.html");
+//     window.location.reload(true);
+// })
