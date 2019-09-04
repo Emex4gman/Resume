ba76909a7d8e6497c58cd8acc81de09f1fa109bf
EMEX <emex4gman@gmail.com>
2019-09-04 13:26:21 +0300

    ....

---
+++ main.js
@@ -0,0 +1,8 @@
+var submit = document.querySelector("input[type=submit]");
+
+submit.addEventListener("click", ()=>{
+    alert("form is not read");
+    window.location.assign("chukwuemeka.html");
+    window.location.reload(true);
+})
+
