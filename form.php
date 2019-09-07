bc9da47ec45900cc380a10f1cdb6e983f572ee42
EMEX <emex4gman@gmail.com>
2019-09-08 00:47:36 +0300

    update

--- form.php
+++ form.php
@@ -1,4 +1,11 @@
 <?php
-
-echo (" you message has been sent ");
+$fullname = $_POST['fullname'];
+$email = $_POST['email'];
+$title = $_POST['title'];
+$message = $_POST['message'];
+$fp = fopen('Data.txt', 'w+');
+$content =  "message form" . PHP_EOL . $title . $fullname . PHP_EOL . "Email: " . $email . PHP_EOL . "Message: " . $message;
+fwrite($fp, $content);
+fclose($fp);
+die(header("Location: " . $_SERVER["HTTP_REFERER"]));
 ?>
