87fd4aafb76d6f39a479bc825bca65c1edf67cf7
CHUKWUEMEKA IBEBUGWU <emex4gman@gmail.com>
2019-09-08 19:46:15 +0300

    update
    update

--- form.php
+++ form.php
@@ -7,5 +7,18 @@ $fp = fopen('Data.txt', 'w+');
 $content =  "message form" . PHP_EOL . $title . $fullname . PHP_EOL . "Email: " . $email . PHP_EOL . "Message: " . $message;
 fwrite($fp, $content);
 fclose($fp);
+ $file = 'Data.txt';
+    
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}
 die(header("Location: " . $_SERVER["HTTP_REFERER"]));
 ?>
